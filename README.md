0559d6803e7ed25f29a31b60237aaa36b42824bc
David Valentiate <me@davidv.ca>
2018-02-06 17:27:42 -0500

    v1.1 ability to specify source commit or branch

--- README.md
+++ README.md
@@ -6,4 +6,34 @@ Assumes that a SSH private key is provided as the environment variable `PRIVATE_
 
 Four required arguments:
 
-    ./action.sh <source_repo_url> <source_branch> <target_repo_url> <target_branch>
+    ./action.sh <source_repo_url> <source_branch_or_commit> <target_repo_url> <target_branch>
+
+## Codeship Usage
+
+`codeship-services.yml` example
+
+```
+heroku-deployment:
+  image: bluedrop360/bitbucket-to-heroku:v1.1
+  encrypted_environment:
+    # ssh private key, public key provided to both bitbucket and heroku
+    # echo -n "PRIVATE_KEY_BASE64=$(pbpaste | base64)" > raw.tmp && jet encrypt raw.tmp crypt.tmp && cat crypt.tmp | pbcopy && rm raw.tmp crypt.tmp
+    - <encypted_value_generated_from_above>
+```
+
+`codeship-steps.yml` example
+
+```
+- name: heroku-deployment
+  tag: master
+  service: heroku-deployment
+  command: /bin/sh -c './action.sh git@bitbucket.org:<org>/<project>.git $CI_COMMIT_ID ssh://git@heroku.com/<app-name>.git master'
+```
+
+**Note:** _The above assumes Codeship cli `jet` utility and using a Mac with the cli copy and paste utilities `pbpaste` and `pbcopy`._
+
+## Version
+
+### v1.1
+
+- Added ability to specify source commit or branch
--- action.sh
+++ action.sh
@@ -9,7 +9,7 @@
 #     ./action.sh <source_repo_url> <source_branch> <target_repo_url> <target_branch>
 
 export SOURCE_REPO="$1"
-export SOURCE_BRANCH="$2"
+export SOURCE_BRANCH_COMMIT="$2"
 export TARGET_REPO="$3"
 export TARGET_BRANCH="$4"
 
@@ -19,11 +19,15 @@ export TARGET_HOST="$(printf '%s' "$TARGET_REPO" | sed 's/.*@//; s/[:/].*//')";
 # use PRIVATE_KEY_BASE64 over PRIVATE_KEY if available
 [ -n "$PRIVATE_KEY_BASE64" ] && export PRIVATE_KEY="$(printf '%s' "$PRIVATE_KEY_BASE64" | base64 -d)"
 
+# get a random 20 character letter/number string as a temporary branch name
+export TEMP_BRANCH="$(cat /dev/urandom | base64 | head -c 1000 | sed 's|[+/]||g' | head -c 20)"
+
 ssh-agent sh -c "printf '%s' \"\$PRIVATE_KEY\" | ssh-add - \
   && ssh -o StrictHostKeyChecking=no \$SOURCE_HOST 'exit' || true \
   && ssh -o StrictHostKeyChecking=no \$TARGET_HOST 'exit' || true \
-  && git clone --branch \"\$SOURCE_BRANCH\" \"\$SOURCE_REPO\" deploy \
+  && git clone -n \"\$SOURCE_REPO\" deploy \
   && cd deploy \
+  && git checkout -b \${TEMP_BRANCH} \"\$SOURCE_BRANCH_COMMIT\" \
   && git remote set-url origin \"\$TARGET_REPO\" \
-  && git push --force origin \"\${SOURCE_BRANCH}:\${TARGET_BRANCH}\" \
+  && git push --force origin \"\${TEMP_BRANCH}:\${TARGET_BRANCH}\" \
   "
